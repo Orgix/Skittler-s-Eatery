9d4488dc2de2512894532af0699c38a41f84d4e5
Nick <tnikos28@gmail.com>
2018-11-23 19:59:42 +0200

    added the new footer

--- resources/templates/front/footer.php
+++ resources/templates/front/footer.php
@@ -6,51 +6,58 @@
 
   <div class="container text-center">
     <div class="row content">
-      <div class="col-md-4 col-sm-12">
+      <div class="col-md-3 order-1 order-md-1 col-sm-12">
         <div class="mb-3 img-logo">
           <a href="#">
-            <img src="logo.png" alt="Skittler's Eatery" media-simple="true">
+            <img src="<?php echo IMG_PATH?>/logo/logo.png" alt="Skittler's Eatery" media-simple="true">
           </a>
         </div>
         <p class="mbr-fonts-style foot-logo display-7">
           Skittler's Eatery
         </p>
       </div>
-      <div class="col-sm-12 d-block d-md-none">
+      <div class="col-md-3 order-2 order-md-4 text-left">
+        <p class="mb-4 text-white font-weight-bold">
+        <u>OPENING HOURS:</u>
+      </p>
+      <p>
+        Mon-Tue: 12am - 11pm
+      </p>
+      <p>
+        Wed: 12am - 10pm
+      </p>
+      <p>
+        Thu-Fri: 11am - 12pm
+      </p>
+      <p>
+        Weekend: 11am - 12pm
+      </p>
 
+      </div>
 
+      <div class="col-sm-12 order-3  mt-2 mt-md-0 d-block d-md-none">
         <div id="accordion" class="accordion">
-      <div class="card mb-0 text-left">
-          <div class="card-header collapsed" data-toggle="collapse" href="#collapseOne">
+          <div class="card mb-0 text-left">
+            <div class="card-header collapsed" data-toggle="collapse" href="#collapseOne">
               <a class="card-title">
                 RECENT NEWS
               </a>
-          </div>
-          <div id="collapseOne" class="card-body collapse" data-parent="#accordion" >
-            <p class="font-weight-bold ">
-              <a href="#" class="text-black">About us</a>
-              <br><a href="#" class="text-black">Services</a>
-              <br><a href="#" class="text-black">Selected Work</a>
-              <br><a href="#" class="text-black">Get In Touch</a>
-              <br><a href="#" class="text-black">Careers</a>
-            </p>
-          </div>
-          <div class="card-header collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+            </div>
+            <div id="collapseOne" class="card-body collapse" data-parent="#accordion">
+
+            </div>
+            <div class="card-header collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
               <a class="card-title">
                 CATEGORIES
               </a>
+            </div>
+            <div id="collapseTwo" class="card-body collapse" data-parent="#accordion">
+
+            </div>
           </div>
-          <div id="collapseTwo" class="card-body collapse" data-parent="#accordion" >
-            <p class=" font-weight-bold">
-              <a href="#" class="text-black">Menu</a>
-              <br><a href="#" class="text-black">Our blog</a>
-              <br><a href="#" class="text-black">Real life</a>
-            </p>
-          </div>
+        </div>
+
       </div>
-  </div>
-
-</div>
 
 
 
@@ -59,29 +66,31 @@
 
 
 
-      <div class="col-md-4 col-sm-4 d-none d-md-block">
+      <div class="col-md-3 col-sm-4 order-md-2 d-none d-md-block">
         <p class="mb-4 text-white font-weight-bold">
           <u>RECENT NEWS</u>
         </p>
         <div id="col_1">
-        <p class="text-light font-weight-bold">
-          <a href="#" class="text-black">About us</a>
-          <br><a href="#" class="text-black">Services</a>
-          <br><a href="#" class="text-black">Selected Work</a>
-          <br><a href="#" class="text-black">Get In Touch</a>
-          <br><a href="#" class="text-black">Careers</a>
-        </p>
+          <p class="text-light font-weight-bold">
+            <a href="#" class="text-black">About us</a>
+            <br><a href="#" class="text-black">Services</a>
+            <br><a href="#" class="text-black">Selected Work</a>
+            <br><a href="#" class="text-black">Get In Touch</a>
+            <br><a href="#" class="text-black">Careers</a>
+          </p>
+        </div>
       </div>
-      </div>
-      <div class="col-md-4 col-sm-4 d-none d-md-block">
+      <div class="col-md-3 col-sm-4 order-md-3 d-none d-md-block">
         <p class="mb-4 text-light font-weight-bold">
           <u>CATEGORIES</u>
         </p>
-        <p class="text-light font-weight-bold" id="col_2">
-          <a href="#" class="text-black">Menu</a>
-          <br><a href="#" class="text-black">Our blog</a>
-          <br><a href="#" class="text-black">Real life</a>
-        </p>
+        <div id="col_2">
+          <p class="text-light font-weight-bold" id="col_2">
+            <a href="#" class="text-black">Menu</a>
+            <br><a href="#" class="text-black">Our blog</a>
+            <br><a href="#" class="text-black">Real life</a>
+          </p>
+        </div>
       </div>
     </div>
     <div class="footer-lower">
@@ -132,12 +141,13 @@
 
 
 
+
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<script src='script.js'></script>
+<script src='js/script.js'></script>
 </body>
 
 </html>
