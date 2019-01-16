1d81eb4d6921be5b6caf038959538038c465e2b5
BasilisTito <45013037+BasilisTito@users.noreply.github.com>
2019-01-17 01:34:08 +0200

    added cart js functions for slider

--- initial_template/js/menu.js
+++ initial_template/js/menu.js
@@ -6,9 +6,9 @@ let row = '';
 
 
 const manageTabs = () =>{ /* Function that closes rest of the tabs that are open. */
-  /* When cliking to open one of the categories in the card-menu, since it's not an accordion, the cards 
+  /* When cliking to open one of the categories in the card-menu, since it's not an accordion, the cards
     remain open , which might result in a tiring user experience. This function is simply responsible for adding
-    the classes that are to be added from the bootstra.js when the open and close operation is called for a card  
+    the classes that are to be added from the bootstra.js when the open and close operation is called for a card
     */
   /*For each menu card*/
   Array.prototype.map.call(menu_cards, card=>{
@@ -22,7 +22,7 @@ const manageTabs = () =>{ /* Function that closes rest of the tabs that are open
       /* add collapsed to the header and the attribute */
       header.classList.add("collapsed")
       header.setAttribute("aria-expanded","false")
-      
+
       /*get the row*/
       row =card.nextSibling.nextSibling
       /* remove and add classes */
@@ -39,9 +39,24 @@ Array.prototype.map.call(menu_cards, card=>{
       currentCard = card.target.classList.contains("h3") ? card.target.parentNode.classList : card.target.classList
       if(currentCard.contains("collapsed")){
         manageTabs()
-      }  
+      }
   });
 });
 
+function openNav() {
+var w= window.innerWidth;
 
-  
+  if (w < 768) {
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("cart-button").style.marginRight = "100%";
+  } else {
+    document.getElementById("mySidenav").style.width = "320px";
+    document.getElementById("cart-button").style.marginRight = "320px";
+  }
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+document.getElementById("mySidenav").style.width = "0";
+document.getElementById("cart-button").style.marginRight = "0"
+}
