72b0a51ef9d162051c9cec157fbc872af8b47020
BasilisTito <45013037+BasilisTito@users.noreply.github.com>
2019-01-18 05:08:16 +0200

    Added cart functions

--- initial_template/js/menu.js
+++ initial_template/js/menu.js
@@ -60,3 +60,52 @@ function closeNav() {
 document.getElementById("mySidenav").style.width = "0";
 document.getElementById("cart-button").style.marginRight = "0"
 }
+
+function addtoCart() {
+  this.style.color ="red";
+  alert(this);
+}
+
+
+/* Jquery */
+$(document).ready(function(){
+
+  $('.menu-tab').click(function() {
+    $('.category_list').show();
+  });
+  $('.checkout-tab').click(function() {
+    $('.category_list').hide();
+  });
+
+  function calculateTotal(){
+		total=0;
+		items=$('.cart-list').find('ul').find('.cart-item');
+		items.each(function(){
+			price=parseFloat($(this).find('.cart-item-cost').html());
+			total += price;
+		});
+		$('.total-cost span:last-child').html(total);
+	}
+
+  $('.add-button').click(function(e){
+		e.preventDefault();
+		var $this = $(this);//this
+    var food_title=$this.parent().parent().find('.food-title').html();//take data from parent li of this
+    var food_description=$this.parent().parent().find('.food-description').html();//take data from parent li of this
+    var food_cost=$this.parent().parent().find('.food-cost').html();
+		var food_cost_init=$this.parent().parent().find('.food-cost').html();
+    $('.cart-list ul').append("<li><div class='card mb-2 cart-item'><div class='card-header'><div class='w-75'><span class='cart-food-title'>"+food_title+
+    "</span><span class='cart-item-cost pl-2'>"+food_cost+"</span></div><span class='remove'>&times;</span></div><div class='card-body'><span class='cart-food-desc'>"+food_description+
+    "</span></div></div></li>"
+    );
+    calculateTotal();
+
+    $('.remove').on("click", function(e){
+      e.preventDefault();
+       $(this).parent().parent().parent().remove();
+       calculateTotal();
+    });
+
+ });
+
+});
